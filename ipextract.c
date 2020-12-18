cd46d73750c0680fce442cccad9b434bcec1b21e
Akshay Gajare <71394698+DeveloperAkshayG@users.noreply.github.com>
2020-12-19 00:28:56 +0530

    Add files via upload
    one file is extracting ip from AT+CIFSR command when AT+CWMODE_CUR=1
    and other is pictorial representation of rows and coulmns of 16*2 lcd

--- ipextract.c
+++ ipextract.c
@@ -11,14 +11,30 @@ Write your code in this editor and press "Run" button to compile and execute it.
 int main()
 {
      char inString[] = "+CIFSR:STAIP,\"192.168.1.117\"";
-     printf("%s\n",inString);
-     char IP[16];
-     char* firstQuote;
-     char* secondQuote;
-     firstQuote = strstr(inString,"\"");
-     secondQuote = strstr(firstQuote+1,"\"");
-     char lengthOfIP = secondQuote - firstQuote - 1;
-     strncpy(IP,firstQuote+1,lengthOfIP);
-     IP[lengthOfIP] = '\0';
-     printf("IP=%s",IP);
+     char ip[20];
+     char buff[50];
+     int k=0;
+     for(int i=0;inString[i] !='\0';i++)
+     {
+        if(inString[i] == '"')
+        do
+        {
+            i++;
+            if(inString[i] == '"')
+            {
+                ip[k]='\0';
+                k++;
+                break;
+            }
+            ip[k] = inString[i];
+            k++;
+            printf("instring[%d]- %c\n",i,inString[i]);
+        //    printf("ip[%d]- %c\n",k,ip[k]);
+        }while(inString[i]!= '"');
+     }
+     printf("the length of ip is %zu\n",strlen(ip));
+     printf("ip is %s\n",ip);
+ //    strncpy(buff,ip,25);
+ //    printf("%s",buff);
+     return 0;
 }
