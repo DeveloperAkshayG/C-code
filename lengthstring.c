435b914715722588b8bc245ea7b103064ecef323
Akshay Gajare <71394698+DeveloperAkshayG@users.noreply.github.com>
2021-07-18 20:26:47 +0530

    Update lengthstring.c
    length of string using strlen and sizeof

--- lengthstring.c
+++ lengthstring.c
@@ -4,6 +4,9 @@
                     
                 sizeof gives me the length of string including null character
                 strlen gives me the length of string exlucding null character    
+                
+            output is:the size using sizeof is 12
+                      the size using strlen is 11    
 *******************************************************************************/
 
 #include <stdio.h>
@@ -13,7 +16,7 @@ int main()
     char str[]="hello world";
     int c=sizeof(str);
     int d=strlen(str);
-    printf("the size using sizeof is %d\n",c);
+    printf("the size using sizeof is %d\n",c); 
     printf("the size using strlen is %d",d);
     return 0;
 }
